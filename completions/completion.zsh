abc35acd27eb2995cca60da52e86814163b7084e
Nishant Joshi <nishant.joshi@juspay.in>
2024-11-05 02:02:11 +0530

    chore: add help in completion

--- completions/completion.zsh
+++ completions/completion.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 _f_counter_completions() {
-    local cur prev opts
+    local cur prev opts output
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
@@ -9,8 +9,15 @@ _f_counter_completions() {
     # All possible options
     opts="-h --help +help -v --version +version +show-date +show-counter +incr"
 
+    # If the user typed "f-counter" and pressed TAB
+    if [[ "$prev" == "f-counter" && -z "$cur" ]]; then
+        output=$(f-counter +help) # Capture the output of the command
+        echo "$output" # Display the output
+        return 0
+    fi
+
     # Filter options based on what the user has typed
-    if [[ ${cur} == -* ]] || [[ ${cur} == +* ]] ; then
+    if [[ ${cur} == -* ]] || [[ ${cur} == +* ]]; then
         # Return all options that match the current input
         COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
         return 0
